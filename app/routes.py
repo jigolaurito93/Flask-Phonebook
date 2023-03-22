3e499c1e123558702d35432ce8baac3d40adee86
Brian <bstanton773@gmail.com>
2023-03-22 11:34:41 -0500

    Handle form submit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import PhoneForm
 
 @app.route('/')
@@ -7,7 +7,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add-phone')
+@app.route('/add-phone', methods=["GET", "POST"])
 def add_phone():
     form = PhoneForm()
+    # Check if the form was submitted and is valid
+    if form.validate_on_submit():
+        first = form.first_name.data
+        last = form.last_name.data
+        address = form.address.data
+        phone = form.phone_number.data
+        print(first, last, address, phone)
+        return redirect(url_for('index'))
     return render_template('add_phone.html', form=form)
