30326b781d007fb16627547c1d086ecf87e9b64d
Jose Laurito <jigolaurito93@yahoo.com>
2023-03-23 03:36:54 -0500

    Add Flask-Migrate and create first migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 # Import the Config class from the config module that has the app configuration like SECRET_KEY, etc.
 from config import Config
 
-# Import the classes form Flask-SQLAlchemy
+# Import the classes form Flask-SQLAlchemy and Flask-Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -11,6 +12,9 @@ app.config.from_object(Config)
 
 # Create an instance of SQLAlchemy to connect to our app to the database
 db = SQLAlchemy(app)
+
+# Create an instance of Migrate that will track our db and app
+migrate = Migrate(app, db)
 
 # import all of the routes from the routes file into the current package 
 from app import routes, models
